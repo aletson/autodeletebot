4e11722b18c8d6855047d91d38a4a6b9c6e0d983
aletson <me@ajl.io>
2023-01-25 15:40:15 -0500

    first pass at halloffame

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const { PermissionsBitField, GatewayIntentBits } = require('discord.js');
+const { PermissionsBitField, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const client = new Discord.Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 var mysql = require('mysql2');
 var connection = mysql.createConnection({
@@ -39,7 +39,25 @@ client.on('ready', async () => {
                     "description": "How many minutes should pass before message removed",
                 }
             ]
-        }];
+        },
+        {
+            "name": "hof",
+            "description": "set up hall of fame functionality",
+            "options": [
+              {
+                "type": 10,
+                "name": "emoji_id",
+                "description": "Right click on the emoji you want and click \"Copy ID\".",
+                "required": true
+              },
+              {
+                "type": 7,
+                "name": "channel",
+                "description": "The channel to set as the Hall of Fame",
+                "required": true
+              }
+            ]
+          }];
         await client.application.commands.set(data);
     }
 });
@@ -68,7 +86,37 @@ client.on('guildCreate', async (guild) => {
                 "description": "How many minutes should pass before message removed",
             }
         ]
-    }];
+    },
+    {
+        "name": "hof",
+        "description": "set up hall of fame functionality",
+        "options": [
+          {
+            "type": 10,
+            "name": "emoji_id",
+            "description": "Right click on the emoji you want and click \"Copy ID\".",
+            "required": true
+          },
+          {
+            "type": 7,
+            "name": "channel",
+            "description": "The channel to set as the Hall of Fame",
+            "required": true
+          }, 
+          {
+            "type": 10,
+            "name": "threshold",
+            "description": "How many people must react",
+            "required": true
+          },
+          {
+            "type": 5,
+            "name": "admin_override",
+            "description": "If a react from an admin automatically HOF's regardless of threshold",
+            "required": true
+          }
+        ]
+      }];
     await guild?.commands.set(data);
 });
 
@@ -94,6 +142,21 @@ client.on('interactionCreate', async (interaction) => {
                     await connection.promise().query('insert into channels (id, enabled, minutes) values (' + channel.id + ',' + enabled + ',' + minutes + ')');
                 }
                 await interaction.reply({ content: 'This probably processed okay!', ephemeral: true });
+            }
+        } else if (interaction.commandName === 'hof') {
+            if (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+                var channel = interaction.options.getChannel('channel');
+                var emoji_id = interaction.options.getInteger('emoji_id');
+                var threshold = interaction.options.getInteger('threshold');
+                var admin_override = interaction.options.getBoolean('admin_override');
+                var isHof = await connection.promise().query('select * from hof where guild_id = ?', interaction.guild.id);
+                var queryData = [channel.id, emoji_id, threshold, admin_override, interaction.guild.id];
+                if(isHof[0].length > 0) {
+                    await connection.promise.query('update hof set channel = ?, emoji = ?, threshold = ?, admin_override = ? where guild_id = ?');
+                } else {
+                    await connection.promise.query('insert into channels (channel, emoji, threshold, admin_override, guild_id) values (?, ?, ?, ?, ?)');
+                }
+                await interaction.reply({content: 'This probably processed okay!', ephemeral: true});
             }
         }
 
@@ -133,3 +196,28 @@ setInterval(async function () {
         }
     }
 }, 600000);
+
+client.on('messageReactionAdd', async function(reaction, user) {
+    //todo cache reactions
+    var hofData = await connection.promise().query('select * from hof where guild_id' = reaction.guild.id);
+    if(hofData[0].length > 0 && reaction.id === hofData[0].emoji_id && (reaction.count >= hofData[0].threshold || (hofData[0].admin_override == true && user.permissions.has(PermissionsBitField.Flags.Administrator)))) {
+        //check message id if already hofed (store)
+        var is_hof = await connection.promise.query('select * from hof_msg where message_id = ?', reaction.message.id); 
+        if (is_hof[0].length <= 0) {
+            //create pin (message embed / rich formatting)
+            const embeddedMessage = newEmbedBuilder()
+            .setColor(0xFFD700)
+            .setAuthor({name: reaction.message.member.displayName})
+            .setDescription(reaction.message.content);
+            if(reaction.message.embeds[0].thumbnail) {
+                embeddedMessage.setImage(embeds[0].thumbnail);
+            }
+            embeddedMessage.setFields({name: 'Source', value: reaction.message.url})
+            .setTimestamp();
+            await message.channel.send({embeds: embeddedMessage, message: reaction.message.channel.toString()});
+            await connection.promise.query('insert into hof_msg (message_id) values (?)', reaction.message.id); 
+        }
+
+
+    } 
+});
